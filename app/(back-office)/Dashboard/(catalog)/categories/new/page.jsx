48208b71df4d447e52151088d96f8b122316df46
multivendor <shaibumansour10@gmail.com>
2024-09-21 23:03:19 +0300

    ep 6

--- app/(back-office)/Dashboard/(catalog)/categories/new/page.jsx
+++ app/(back-office)/Dashboard/(catalog)/categories/new/page.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 export default function NewCategory() {
     return (
         <div>
-            <h2>New Category</h2>
+            <div className="flex items-center justify-between
+            px-12 py-6 bg-white text-slate-800 dark:text-slate-50 
+             dark:bg-slate-600 rounded-lg shadow ">
+                <h2 className="text-xl font-semibold">New Category</h2>
+                <button className="">
+                    <x />
+                </button>
+
+            </div>
+            {/*
+            id
+            title
+            slag
+            description
+            image
+            
+            */}
+
         </div>
 
     )
